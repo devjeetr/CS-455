8dbd55b1dc8cbf0a3cf679a8c3564d2d040e2526
Devjeet Raj Roy <devjeetroy@localhost.localdomain>
2016-11-13 04:02:29 -0800

    Cleaned up repository structure

---
+++ Project 1/mk.sh
@@ -0,0 +1,8 @@
+echo Building bin/project1Server
+gcc -std=c99 -w src/project1Server.c -o bin/project1Server
+
+echo Building bin/project1Client
+gcc -std=c99 -w src/project1Client.c -o bin/project1Client
+
+echo Build Succeeded. 
+
--- src/mk.sh
+++
@@ -1,2 +0,0 @@
-gcc -std=c99 project1Server.c -o project1Server
-gcc -std=c99 project1Client.c -o project1Server
