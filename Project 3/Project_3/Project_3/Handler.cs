7b1a6c9ee1b1ae0d4d9e93b251148ef24fe438be
Devjeet Roy <devjeetrr@gmail.com>
2016-12-08 19:25:02 -0800

    Removed unnecessary files

--- Project 3/Project_3/Project_3/Handler.cs
+++ Project 3/Project_3/Project_3/Handler.cs
@@ -79,17 +79,17 @@ namespace Project_3
 		}
 
 
-		public static void connect(Socket serverSocket, string header) {
-			Console.WriteLine("Connecting to remote server");
 
+		public static void connect(Socket serverSocket, string header) {
+			// Console.WriteLine("Connecting to remote server");
 			//Build C_Socket (client Socket)
 			Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
 			var parsedHeaders = parseHeaders(header);
 			//Console.WriteLine(header);
 			if (!parsedHeaders.ContainsKey("Host")) {
-				Console.WriteLine("No host header");
-				Console.WriteLine(header);
+				//Console.WriteLine("No host header");
+				//Console.WriteLine(header);
 				clientSocket.Close();
 				serverSocket.Close();
 				//serverSocket.Shutdown(SocketShutdown.Send);
@@ -99,22 +99,19 @@ namespace Project_3
 
 			var domain = String.Format("{0}.{1}", url[url.Length - 2], url[url.Length - 1]);
 			IPAddress[] addresslist;
+
 			try
 			{
 				addresslist = Dns.GetHostAddresses(domain);
-			}
-			catch (Exception){
+			} catch (Exception){
 				Console.WriteLine("Exception");
 				clientSocket.Close();
 				serverSocket.Close();
 				return;
 			}
-			//Console.WriteLine("ip: {0}", addresslist[0]);
-			//Console.WriteLine(url);
+
 			IPEndPoint remoteEP = new IPEndPoint(addresslist[0], 80);
 			clientSocket.Connect(remoteEP);
-
-
 
 			//Send Client Request to the real server
 			SendRequest(clientSocket, header);
@@ -123,53 +120,126 @@ namespace Project_3
 			header = GetHeader(clientSocket);
 
 			var clientHeadersParsed = parseHeaders(header);
-			if (!clientHeadersParsed.ContainsKey("Content-Length"))
+			SendRequest(serverSocket, header);
+
+			if (clientHeadersParsed.ContainsKey("Content-Length"))
 			{
-				Console.WriteLine("No hostcontent lenght");
-				Console.WriteLine(header);
+				var contentLength = int.Parse(clientHeadersParsed["Content-Length"]);
+				ProcessWithContentLength(serverSocket, clientSocket, header, contentLength);
+			}
+			else if (clientHeadersParsed.ContainsKey("Transfer-Encoding"))
+			{
+
+				RecieveChunks(serverSocket, clientSocket);
+				Console.WriteLine("Receiving chunks");
+			} else { 
+				//Console.WriteLine("No hostcontent lenght");
+				//Console.WriteLine(header);
 				clientSocket.Close();
 				serverSocket.Close();
-				//serverSocket.Shutdown(SocketShutdown.Send);
 				return;
 			}
-			var contentLength = int.Parse(clientHeadersParsed["Content-Length"]);
-			//Send the real server response to the client through server socket
-			SendRequest(serverSocket, header);
-			int receivedBytes = 0;
-			const int BUFSZ = 1024;
-			byte[] buffer = new byte[BUFSZ];
-
-			//var recvHeader = GetHeader(clientSocket);
-			//var bytes = System.Text.Encoding.ASCII.GetBytes(recvHeader);
-
-			//Console.WriteLine(recvHeader);
-			receivedBytes = clientSocket.Receive(buffer);
-			contentLength -= receivedBytes;
-			//Buffer.BlockCopy(bytes, 0, buffer, 0, receivedBytes);
-
-			// TODO
-			// take into account content length
-			while (receivedBytes > 0 && contentLength > 0)
-			{
-				Console.WriteLine("Sending... {0} bytes", receivedBytes);
-				//Console.WriteLine(System.Text.Encoding.ASCII.GetString(buffer));
-				//Console.WriteLine("-------------");
-
-
-				serverSocket.Send(buffer, receivedBytes, SocketFlags.None);
-				receivedBytes = clientSocket.Receive(buffer);
-				contentLength -= receivedBytes;
-
-			}
-
-			//Shutdown and close the server socket
-			//serverSocket.
 
 			//serverSocket.Shutdown(SocketShutdown.Both);
 			serverSocket.Close();
 			clientSocket.Close();
-			//clientSocket.Shutdown(SocketShutdown.Both);
+			// clientSocket.Shutdown(SocketShutdown.Both);
 			Console.WriteLine("Ending");
+		}
+
+		public static void ProcessWithTransferEncoding(Socket serverSocket, Socket clientSocket,
+													string clientHeader) { 
+			
+		
+		}
+
+		private static string getNextChunk(Socket socket, int chunkSize) { 
+			string header = "";
+			chunkSize += "\r\n".Length;
+			while (chunkSize > 0)
+			{
+				byte[] bytes = new byte[1];
+				int bytesRec = socket.Receive(bytes);
+				header += System.Text.Encoding.UTF8.GetString(bytes, 0, bytesRec);
+				if (header.IndexOf("\r\n") > -1 || header.IndexOf("\n\n") > -1)
+				{
+					break;
+				}
+				chunkSize--;
+			}
+			return header;
+		}
+
+		private static int GetChunkSize(Socket socket) { 
+			string header = "";
+
+			while (true)
+			{
+				Console.WriteLine("recving chunk size");
+
+				byte[] bytes = new byte[1];
+				int bytesRec = socket.Receive(bytes);
+
+				header += System.Text.Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+				Console.WriteLine(header);
+
+				if (header.IndexOf("\r\n") > -1 || header.IndexOf("\n\n") > -1)
+				{
+					break;
+				}
+			}
+
+			int num = Int32.Parse(header, System.Globalization.NumberStyles.HexNumber);
+			Console.WriteLine("ChunkSize: {0}", num);
+
+
+			return num;
+		}
+
+		private static byte[] RecieveChunks(Socket serverSocket, Socket clientSocket) {
+
+			var data = "";
+
+			int size = GetChunkSize(clientSocket);
+			var chunk = getNextChunk(clientSocket, size);
+			data += chunk;
+
+			while (chunk.Length > 0) {
+				try
+				{
+					size = GetChunkSize(clientSocket);
+				}
+				catch (FormatException e){
+					Console.WriteLine("Format exception");
+					break;
+				}
+				chunk = getNextChunk(clientSocket, size);
+				serverSocket.Send(System.Text.Encoding.UTF8.GetBytes(chunk), size, SocketFlags.None);
+				data += chunk;
+			}
+
+			return System.Text.Encoding.ASCII.GetBytes(data);
+		}
+
+		public static void ProcessWithContentLength(Socket serverSocket, Socket clientSocket, 
+		                                            string clientHeader, int contentLength) { 
+		
+			//Send the real server response to the client through server socket
+
+			int receivedBytes = 0;
+			const int BUFSZ = 1024;
+			byte[] buffer = new byte[BUFSZ];
+
+			receivedBytes = clientSocket.Receive(buffer);
+			contentLength -= receivedBytes;
+
+			while (receivedBytes > 0 && contentLength > 0)
+			{
+				serverSocket.Send(buffer, receivedBytes, SocketFlags.None);
+				receivedBytes = clientSocket.Receive(buffer);
+				contentLength -= receivedBytes;
+			}
 		}
 
 		public static void SendRequest(Socket socket, string header)
